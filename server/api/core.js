1b0ec4c2a6e7d5808e466d96753396acc2914ac7
Nikolaus Piccolotto <prayerslayer@gmail.com>
2015-11-08 20:45:34 +0100

    Cache presigned S3 url

--- server/api/core.js
+++ server/api/core.js
@@ -3,7 +3,26 @@ var pg = require('pg'),
     DB_URL = process.env.DB_URL,
     aws = require('aws-sdk'),
     s3 = new aws.S3(),
-    db = sqlt.dir('../db');
+    db = sqlt.dir('../db'),
+    URL_CACHE = {};
+
+function getImageUrl(ui) {
+    if (!URL_CACHE[ui] || URL_CACHE[ui].expires < Date.now()) {
+        // CACHE MISS
+        // HULK SMASH
+        var url = s3.getSignedUrl('getObject',{
+            Bucket: process.env.S3_BUCKET,
+            Key: ui,
+            Expires: 3600 // keep for an hour
+        });
+        URL_CACHE[ui] = {
+            url: url,
+            expires: Date.now() + 1000 * 60 * 60
+        };
+        return url;
+    }
+    return URL_CACHE[ui].url;
+}
 
 module.exports.getImage = (app) => {
     // TODO cache
@@ -32,14 +51,11 @@ module.exports.getUIs = () => {
                 if (err2) {
                     return reject(err2);
                 }
-                //TODO cache signed urls, so that the browser can too
-                var result = result.rows
-                                .map(result => {
-                                    result.image_url = s3.getSignedUrl('getObject',{
-                                        Bucket: process.env.S3_BUCKET,
-                                        Key: result.id
-                                    });
-                                    return result;
+                var result = result
+                                .rows
+                                .map(row => {
+                                    row.image_url = getImageUrl(row.id);
+                                    return row;
                                 });
                 resolve(result);
             });
