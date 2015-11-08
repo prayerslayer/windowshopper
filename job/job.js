d11509d130c2efc44eb2e2656cc7f01a5445a78d
Nikolaus Piccolotto <prayerslayer@gmail.com>
2015-11-08 21:57:22 +0100

    Do not rely on npm phantom
    It's really sad the installation of this thing is so flaky :sob:

--- job/job.js
+++ job/job.js
@@ -87,12 +87,12 @@ function saveScreenshot(app) {
     .get(url)
     .end((err, res) => {
         if (err) {
-            winston.error('Could not read', url, err);
+            winston.error('Could not read', url, err.message);
             return;
         }
         // we lose ASCII art UIs here, but well...
         if (res.ok && /text\/html/.test(res.headers['content-type'])) {
-            shell.exec('../node_modules/phantomjs2/bin/phantomjs save.js ' + id + ' ' + url);
+            shell.exec('./phantomjs save.js ' + id + ' ' + url);
             winston.info('Downloaded', id);
             persistImages(id, url);
         } else {
